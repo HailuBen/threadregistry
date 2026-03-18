1afa4223b348dcb3835dad3c8523cfe503cc5634
HailuBen <b.hailu.pk@gmail.com>
2026-03-18 18:38:45 -0400

    update, add lookup functionality

--- src/main/java/com/example/demo/repositories/BrandRepository.java
+++ src/main/java/com/example/demo/repositories/BrandRepository.java
@@ -2,8 +2,15 @@ package com.example.demo.repositories;
 
 import com.example.demo.models.Brand;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
+@Repository
 public interface BrandRepository extends JpaRepository<Brand, Long> {
 
     boolean existsByName(String name);
+
+    // Allows looking up "Nike" to get the Brand object
+    Optional<Brand> findByName(String name);
 }
